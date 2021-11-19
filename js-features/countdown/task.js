f71bf1b51c819236707bc26a31d0b71e2eeac4c7
AlexKranov <86122490+AlexKranov@users.noreply.github.com>
2021-11-19 12:06:17 +0300

    update timer

---
+++ js-features/countdown/task.js
@@ -0,0 +1,13 @@
+let timer; // пока пустая переменная
+let x = 59; // стартовое значение обратного отсчета
+countdown(); // вызов функции
+function countdown() { // функция обратного отсчета
+    document.getElementById("timer").innerHTML = x;
+    x--; // уменьшаем число на единицу
+    if (x < 0) {
+        clearTimeout(timer); // таймер остановится на нуле
+        alert("«Вы победили в конкурсе!»");
+    } else {
+        timer = setTimeout(countdown, 1000);
+    }
+}
