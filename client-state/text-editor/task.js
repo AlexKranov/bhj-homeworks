41700c46a210912dd20998d97f822758568b20b3
AlexKranov <86122490+AlexKranov@users.noreply.github.com>
2021-12-21 10:07:09 +0300

    update

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,13 @@
+const textArea = document.getElementById('editor');
+const clear = document.getElementById('clear');
+
+textArea.addEventListener('keyup', event => {
+    localStorage.setItem('text', textArea.value);
+})
+
+textArea.value = localStorage.getItem('text');
+
+clear.addEventListener('click', event => {
+    localStorage.removeItem('text');
+    textArea.value = '';
+})
